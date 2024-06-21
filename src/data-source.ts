51a73b9e11e16de296d5d14d0079b34582358d82
ZingerEngineer <ramy.yasser21102@gmail.com>
2024-06-22 01:52:03 +0300

    Fixed db connection config.

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,12 @@
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 import { User } from './entity/User'
+import dotenv from 'dotenv'
 
+dotenv.config()
+const database = process.env.DB_NAME_SECRET
 const username = process.env.DB_USER_NAME_SECRET
+const password = process.env.DB_USER_PASS_SECRET
 const port = parseInt(process.env.DB_PORT_SECRET)
 let synchronize = false
 if (process.env.NODE_ENV == 'development') synchronize = true
@@ -12,12 +16,15 @@ export const AppDataSource = new DataSource({
   host: 'localhost',
   port,
   username,
-  password: '',
-  database: 'sports_club',
+  password,
+  database,
   synchronize: synchronize,
   logging: false,
   entities: [User],
   migrations: [],
-  subscribers: []
+  subscribers: [],
+  extra: {
+    trustServerCertificate: true
+  }
 })
 
