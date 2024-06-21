af5f82482078bb57bebe1ce55693d7aede01035f
ZingerEngineer <ramy.yasser21102@gmail.com>
2024-06-22 01:52:36 +0300

    Handled db connection promise.

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,13 @@ import router from './routers/router'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 import { AppDataSource } from './data-source'
-import { User } from './entity/User'
 
 dotenv.config()
 
-AppDataSource.initialize()
+export const AppDataSourceRes = AppDataSource.initialize()
   .then(async () => {
-    console.log('Inserting a new user into the database...')
-    const user = new User()
-    user.firstName = 'Timber'
-    user.lastName = 'Saw'
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log('Saved a new user with id: ' + user.id)
-
-    console.log('Loading users from the database...')
-    const users = await AppDataSource.manager.find(User)
-    console.log('Loaded users: ', users)
-
-    console.log(
-      'Here you can setup and run express / fastify / any other framework.'
-    )
+    console.log('Connected to DB.')
+    return AppDataSource
   })
   .catch((error) => console.log(error))
 
