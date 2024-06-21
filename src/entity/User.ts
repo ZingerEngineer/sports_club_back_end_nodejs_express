d86451c4f9a62f1aa855697b99825233ca8c6171
ZingerEngineer <ramy.yasser21102@gmail.com>
2024-06-22 01:53:08 +0300

    Modified user entity *WIP

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,35 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  user_id: number
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column()
+  role: string
 
-    @Column()
-    firstName: string
+  @Column()
+  firstName: string
 
-    @Column()
-    lastName: string
+  @Column()
+  lastName: string
 
-    @Column()
-    age: number
+  @Column()
+  team: number
 
+  @Column()
+  dob: Date
+
+  @Column()
+  job: string
+
+  @Column()
+  reports_from: number
+
+  @Column()
+  reported_on: number
+
+  @Column()
+  isDeleted: boolean
 }
+
