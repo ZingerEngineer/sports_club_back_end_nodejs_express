dbc9a7dd5f7ea68bc4da32572b17ec30d26eb218
ZingerEngineer <ramy.yasser21102@gmail.com>
2024-07-03 18:19:24 +0300

    *DN Fixed new checkid change.

--- src/controllers/sport.controller.ts
+++ src/controllers/sport.controller.ts
@@ -9,15 +9,15 @@ export class SportController {
   //Finding a sport by ID.
 
   async findSportById(id: number | string) {
-    const checkedId = checkIdValidity(id)
-    if (checkedId) {
-      return sportRepository.findOneBy({ sport_id: checkedId })
-    }
+    const checkRes = checkIdValidity(id)
+    if (checkRes === 0) return 0
+    const checkedId = checkRes.id
+    return sportRepository.findOneBy({ sport_id: checkedId, is_deleted: 0 })
   }
 
   //Finding a sport with name.
   async findSportByName(sport_name: string) {
     if (null === sport_name || undefined === sport_name) return -1
-    return sportRepository.findOneBy({ sport_name: sport_name })
+    return sportRepository.findOneBy({ sport_name: sport_name, is_deleted: 0 })
   }
 }
