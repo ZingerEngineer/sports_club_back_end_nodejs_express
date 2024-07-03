93a4446d0c2347ef1776d72fe914b4a3b42c7ec5
ZingerEngineer <ramy.yasser21102@gmail.com>
2024-07-03 18:21:23 +0300

    *DN Improved checkIdValidity to return object with validity bit for conditions.

--- src/utils/checkIdValidity.ts
+++ src/utils/checkIdValidity.ts
@@ -1,12 +1,11 @@
 export const checkIdValidity = (id: string | number) => {
   if (null === id || undefined === id || NaN) return 0
   if (typeof id === 'string') {
-    if (Object.is(parseInt(id), NaN)) {
-      return 0
-    } else {
-      return parseInt(id)
-    }
+    if (Object.is(parseInt(id), NaN)) return 0
+    if (!Number.isInteger(Number(id))) return 0
+    return { id: parseInt(id), valid: 1 }
   } else {
-    return id
+    if (!Number.isInteger(id)) return 0
+    return { id, valid: 1 }
   }
 }
