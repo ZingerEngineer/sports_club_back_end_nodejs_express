590f17365036551c22705f53c5a2a1440b8c62ae
ZingerEngineer <ramy.yasser21102@gmail.com>
2024-07-03 18:19:05 +0300

    *DN Sponsor controller & fixed new check id.

--- src/controllers/sponsor.controller.ts
+++ src/controllers/sponsor.controller.ts
@@ -9,15 +9,18 @@ export class SponsorController {
   //Finding a sport by ID.
 
   async findSponsorById(id: number | string) {
-    const checkedId = checkIdValidity(id)
-    if (checkedId) {
-      return sponsorRepository.findOneBy({ sponsor_id: checkedId })
-    }
+    const checkRes = checkIdValidity(id)
+    if (checkRes === 0) return 0
+    const checkedId = checkRes.id
+    return sponsorRepository.findOneBy({ sponsor_id: checkedId, is_deleted: 0 })
   }
 
   //Finding a sport with name.
   async findSponsorByName(sponsor_name: string) {
     if (null === sponsor_name || undefined === sponsor_name) return -1
-    return sponsorRepository.findOneBy({ brand_name: sponsor_name })
+    return sponsorRepository.findOneBy({
+      brand_name: sponsor_name,
+      is_deleted: 0
+    })
   }
 }
