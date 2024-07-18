07551d78ddfd56acd5ad6cfa7fb856356691637f
ZingerEngineer <ramy.yasser21102@gmail.com>
2024-07-18 22:15:29 +0300

    Added new controller functions (endpoints) for the public router.

--- src/routes/publicRouter.ts
+++ src/routes/publicRouter.ts
@@ -1,8 +1,31 @@
 import express from 'express'
 import { test } from '../controllers/test.controller'
+import {
+  loginController,
+  signUpController
+} from '../controllers/user.controller'
+import { validationMiddleWare } from '../middlewares/validation.middleware'
+import {
+  userLoginPayLoadSchema,
+  userSignUpPayLoadSchema
+} from '../validationSchemas/user'
+
+import multer from 'multer'
+const upload = multer({ dest: 'uploads/' })
 
 const publicRouter = express.Router()
 
-publicRouter.use('/test', test)
+publicRouter.get('/test', test)
+publicRouter.post(
+  '/signup',
+  upload.none(),
+  validationMiddleWare(userSignUpPayLoadSchema),
+  signUpController
+)
+publicRouter.post(
+  '/login',
+  validationMiddleWare(userLoginPayLoadSchema),
+  loginController
+)
 
 export default publicRouter
