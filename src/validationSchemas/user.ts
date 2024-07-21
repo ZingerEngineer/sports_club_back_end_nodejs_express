947a7f92f2529b2d457b570cb49d0232e7657452
ZingerEngineer <ramy.yasser21102@gmail.com>
2024-07-21 21:46:39 +0300

    Removed zod type (unnecessary).

--- src/validationSchemas/user.ts
+++ src/validationSchemas/user.ts
@@ -1,4 +1,4 @@
-import { z, ZodType } from 'zod'
+import { z } from 'zod'
 
 const userSignUpPayLoadSchema = z
   .object({
@@ -28,18 +28,20 @@ const userSignUpPayLoadSchema = z
       .trim()
       .min(1, { message: 'A password name is required.' })
       .max(45, { message: 'Password exceeded the maximum length.' }),
-    confirmPassword: z.string(),
+    confirmPassword: z
+      .string()
+      .trim()
+      .min(1, { message: 'A password name is required.' })
+      .max(45, { message: 'Password exceeded the maximum length.' }),
     gender: z
-      .string()
-      .trim()
-      .min(1, { message: 'A gender is required.' })
-      .max(6, { message: 'Invalid gender.' }),
-    dob: z.date({ message: 'Invaid date.' }),
+      .number()
+      .min(0, { message: 'Invalid gender.' })
+      .max(1, { message: 'Invalid gender.' }),
+    dob: z.string().date('Invalid date.'),
     job: z
-      .string()
-      .trim()
-      .min(1, { message: 'A job is required.' })
-      .max(12, { message: 'Invalid job.' })
+      .number()
+      .min(0, { message: 'Invalid job.' })
+      .max(2, { message: 'Invalid job.' })
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: 'Passwords do not match',
@@ -53,22 +55,22 @@ const userLoginPayLoadSchema = z
       .trim()
       .email()
       .min(1, { message: 'An email is required.' })
-      .max(200, { message: 'Invalid email address' }),
+      .max(200, { message: 'Invalid email address' })
+      .optional(),
     phone: z
       .string()
       .trim()
       .min(1, { message: 'A phone number is required.' })
-      .max(15, { message: 'Invalid phone number.' }),
+      .max(15, { message: 'Invalid phone number.' })
+      .optional(),
     password: z
       .string()
       .trim()
       .min(1, { message: 'A password name is required.' })
-      .max(45, { message: 'Password exceeded the maximum length.' }),
-    confirmPassword: z.string()
+      .max(45, { message: 'Password exceeded the maximum length.' })
   })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: 'Passwords do not match',
-    path: ['confirmPassword'] // path of error
+  .refine((data) => data.email || data.phone, {
+    message: 'Either phone or email must be provided.'
   })
 
 export { userLoginPayLoadSchema, userSignUpPayLoadSchema }
