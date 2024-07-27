5b80c051d324d512328bdc2d052f71af35fc9fed
ZingerEngineer <ramyisez@gmail.com>
2024-07-28 00:28:11 +0300

    Optimized the session create logic.

--- src/repositories/session.repository.ts
+++ src/repositories/session.repository.ts
@@ -2,6 +2,7 @@ import { AppDataSource } from '../services/data-source'
 import { Session } from '../entities/Session'
 import crypto from 'crypto'
 import { User } from '../entities/User'
+import { addMinutesToDate } from '../utils/addMinutesToDate'
 
 export const sessionRepository = AppDataSource.getRepository(Session).extend({
   async createSession(user: User, expiresAt?: string, sessionData?: string) {
@@ -14,9 +15,16 @@ export const sessionRepository = AppDataSource.getRepository(Session).extend({
 
       if (expiresAt) {
         sessionObject['expiresAt'] = expiresAt
+      } else {
+        sessionObject['expiresAt'] = addMinutesToDate(
+          new Date(),
+          24 * 60
+        ).toISOString()
       }
       if (sessionData) {
         sessionObject['data'] = sessionData
+      } else {
+        sessionObject['data'] = ''
       }
       const newSession = sessionRepository.create(sessionObject)
       return await sessionRepository.save(newSession)
