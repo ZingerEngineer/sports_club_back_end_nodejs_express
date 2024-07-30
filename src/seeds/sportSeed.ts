364635ee836db654225abd4f998fcd9d78cd553d
ZingerEngineer <ramy.yasser21102@gmail.com>
2024-07-30 21:34:15 +0300

    Created sport and team seeding scripts to seed data into the db.

---
+++ src/seeds/sportSeed.ts
@@ -0,0 +1,38 @@
+import { Sport } from '../entities/Sport'
+import { sportRepository } from '../repositories/sport.repository'
+import { AppDataSource } from '../services/data-source'
+
+const sports: Partial<Sport>[] = [
+  {
+    name: 'Football',
+    description: 'Players play a game where they kick a black & white ball.',
+    rules: 'Only touch the ball with your feet.'
+  },
+  {
+    name: 'Basketball',
+    description: 'Players play a game where they bounce an orange ball.',
+    rules: 'You have to keep dribbling the ball.'
+  }
+]
+
+export const sportSeeder = async (sportSeed: Partial<Sport>[] = sports) => {
+  try {
+    await AppDataSource.initialize()
+    console.log('Sport seeder connected to db.')
+    for (const sport of sportSeed) {
+      sportRepository.create({
+        name: sport.name,
+        description: sport.description,
+        rules: sport.rules
+      })
+      await sportRepository.save(sport)
+    }
+    console.log(`Sport seeding is done.`)
+  } catch (error) {
+    console.log(`Sport seeding failed due to error: ${error}`)
+  }
+}
+;(async () => {
+  await sportSeeder()
+  return
+})()
---
+++ src/seeds/teamSeed.ts
@@ -0,0 +1,47 @@
+import { Team } from '../entities/Team'
+import { TeamType } from '../enums/team.enums'
+import { sportRepository } from '../repositories/sport.repository'
+import { teamRepository } from '../repositories/team.repository'
+import { AppDataSource } from '../services/data-source'
+
+const teams = [
+  {
+    teamName: 'Titans',
+    teamType: TeamType.CANDIDATES,
+    belowAge: 15,
+    sportName: 'Football'
+  },
+  {
+    teamName: 'Lackers',
+    teamType: TeamType.AMATURES,
+    belowAge: 26,
+    sportName: 'Basketball'
+  }
+]
+
+export const teamSeeder = async (teamSeed = teams) => {
+  try {
+    await AppDataSource.initialize()
+    console.log('Team seeder connected to db.')
+
+    for (const team of teamSeed) {
+      const dbSport = await sportRepository.findOneBy({
+        name: team.sportName
+      })
+      teamRepository.create({
+        teamName: team.teamName,
+        teamType: team.teamType,
+        belowAge: team.belowAge,
+        sport: dbSport
+      })
+      await teamRepository.save(team)
+    }
+    console.log(`Team seeding is done.`)
+  } catch (error) {
+    console.log(`Team seeding failed due to error:${error}`)
+  }
+}
+;(async () => {
+  await teamSeeder()
+  return
+})()
