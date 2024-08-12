d048e482a03f27101364d773bae94128d4708680
ZingerEngineer <ramyisez@gmail.com>
2024-08-13 02:05:40 +0300

    Removed unnecessary token repo custom methods.

--- src/repositories/token.repository.ts
+++ src/repositories/token.repository.ts
@@ -1,14 +1,4 @@
 import { AppDataSource } from '../services/data-source'
 import { Token } from '../entities/Token'
-import { addMinutesToDate } from '../utils/addMinutesToDate'
-import { User } from '../entities/User'
 
-export const tokenRepository = AppDataSource.getRepository(Token).extend({
-  async deleteToken(token: string) {
-    tokenRepository
-      .createQueryBuilder('token')
-      .delete()
-      .from(Token)
-      .where('token.token = :tokenBody', { tokenBody: token })
-  }
-})
+export const tokenRepository = AppDataSource.getRepository(Token)
